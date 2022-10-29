1f4263e8c85f793da1b72d5ab93b1838d48dd0ba
Gaurav Jeena <gauravjeena@outlook.com>
2022-10-29 10:21:35 +0000

    test if ssh key worked

--- conditional.rb
+++ conditional.rb
@@ -3,10 +3,11 @@ a = gets.chomp.to_i
 
 if a == 3
   puts "a is 3"
-  elsif a == 4
+elsif a == 4
     puts "a is 4"
-  else
+else
     puts "a is neither 3, nor 4"
     
 end
 puts "A"
+puts "testing if ssh key worked"
